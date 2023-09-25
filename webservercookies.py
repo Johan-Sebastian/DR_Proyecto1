16b736a70cdd6641e5626e7e62cbc2da42f961ef
Johan-Sebastian <80274564+Johan-Sebastian@users.noreply.github.com>
2023-09-25 01:27:04 -0700

    Update webservercookies.py

--- webservercookies.py
+++ webservercookies.py
@@ -1,10 +1,11 @@
 from functools import cached_property
 from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import parse_qsl, urlparse, unquote
+from urllib.parse import parse_qsl, urlparse
+from bs4 import BeautifulSoup as BS
 import re
 import redis
-import uuid
+import uuid 
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
@@ -35,6 +36,7 @@ class WebRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         method = self.get_method(self.url.path)
+        print("Method", method)
         if method:
             method_name, dict_params = method
             method = getattr(self, method_name)
@@ -57,6 +59,7 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         session_id = self.get_book_session()
         book_recomendation = self.get_book_recomendation(session_id, book_id)
         book_page = r.get(book_id)
+        print(book_id)
         if book_page:
             self.send_response(200)
             self.send_header("Content-Type", "text/html")
@@ -85,57 +88,41 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response.encode("utf-8"))
 
     def get_method(self, path):
+        print(path)
         for pattern, method in mapping:
             match = re.match(pattern, path)
             if match:
                 return (method, match.groupdict())
 
-    def get_search(self, query):
-		session_id = self.get_book_session()
-		matching_books = self.search_books_in_redis(query)
-		search_results_html = self.generate_search_results_html(matching_books)
-		self.send_response(200)
-		self.send_header("Content-Type", "text/html")
-		self.set_book_cookie(session_id)
-		self.end_headers()
-		self.wfile.write(search_results_html.encode("utf-8"))
-
-	def search_books_in_redis(self, query):
-		# Código para buscar libros en Redis según el término de búsqueda 'query'
-    	# Supongamos que los libros están almacenados en un conjunto llamado "libros"
-    	matching_books = []
-    
-    	# Obtén todos los libros desde Redis (esto depende de cómo estén almacenados tus datos en Redis)
-    	all_books = r.smembers("libros")
-    
-    	# Itera a través de los libros y verifica si el término de búsqueda está en el título o autor
-    	for book_data in all_books:
-    		book = book_data.decode('utf-8')  # Decodifica los datos del libro
-    		if query.lower() in book.lower():
-    			title, author = book.split('|')  # Supongamos que los datos del libro están separados por '|'
-    			matching_books.append({'title': title, 'author': author})
-    
-    	return matching_books
-
-	def generate_search_results_html(self, matching_books):
-		# Genera el HTML de los resultados de búsqueda
-    	search_results_html = "<h2>Resultados de la búsqueda:</h2>"
-    	if not matching_books:
-    		search_results_html += "<p>No se encontraron resultados.</p>"
-    	else:
-    		search_results_html += "<ul>"
-    		for book in matching_books:
-    			search_results_html += f"<li>{book['title']} - {book['author']}</li>"
-    		search_results_html += "</ul>"
-    	return search_results_html
-
+    def get_search(self, *srch):
+        searchpage = r.get("search")
+        searchquery = self.url.query[5:]
+        lastadd = ""
+        for id in range(5):
+            html = r.get(id + 1).decode()
+            text = BS(html, 'html.parser').get_text()
+            if re.search(searchquery, text):
+                print("D")
+                lastadd = lastadd + f"""
+                      <h1><a href="\\books\\{id+1}"> Libro {id+1} contiene busqueda</a></h1><br>
+                      """
+        self.send_response(200)
+        self.send_header("Content-Type","text/html")
+        self.end_headers()
+        print(self.url.query)
+        response=f"""
+            {searchpage.decode()}
+            """ + lastadd
+        self.wfile.write(response.encode("utf-8"))
 
 mapping = [
             (r'^/books/(?P<book_id>\d+)$', 'get_book'),
-            (r'^/$', 'get_index')
+            (r'^/$', 'get_index'),
+            (r'^/search', 'get_search')
         ]
 
 if __name__ == "__main__":
     print("Server starting...")
-    server = HTTPServer(("0.0.0.0", 8000), WebRequestHandler)
+    server = HTTPServer(("0.0.0.0", 80), WebRequestHandler)
+    print("Up")
     server.serve_forever()
