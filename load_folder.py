1f86fd15538df775984f1ddb83ce2e64e1e659da
Johan-Sebastian <80274564+Johan-Sebastian@users.noreply.github.com>
2023-09-25 00:55:53 -0700

    Update load_folder.py

--- load_folder.py
+++ load_folder.py
@@ -1,8 +1,10 @@
 import os
 import redis
 import re
+# https://github.com/redis/redis-py
 
 r = redis.Redis(host='localhost', port=6379, db=0)
+
 
 def load_folder(path):
     files = os.listdir(path)
@@ -10,10 +12,10 @@ def load_folder(path):
     for file in files:
         match = re.match(r'^book(\d+).html$', file)
         if match:
-            with open(os.path.join(path, file), 'r', encoding='utf-8') as f:
-                html_content = f.read()
-                book_id = match.group(1)
-                r.set(f"book_{book_id}", html_content)
-                print(f"Loaded book_{book_id} into Redis")
+            with open(path + file) as f:
+                html = f.read()
+                r.set(match.group(1), html)
+            print(match.group(0), match.group(1))
+
 
 load_folder('html/books/')
