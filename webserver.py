41df7a93aaf9f44eef2dcf2b797fb0573654d51b
Johan-Sebastian <80274564+Johan-Sebastian@users.noreply.github.com>
2023-09-24 22:32:48 -0700

    Update webserver.py
    En la clase WebRequestHandler, agrega un nuevo método llamado search_books que manejará las solicitudes de búsqueda. También, agrega un nuevo patrón de ruta a la lista mapping para asociar las solicitudes de búsqueda a este método. Aquí tienes una implementación de cómo podría ser

--- webserver.py
+++ webserver.py
@@ -10,6 +10,37 @@ from urllib.parse import parse_qsl, urlparse
 
 
 class WebRequestHandler(BaseHTTPRequestHandler):
+############################################################################################
+        def search_books(self):
+        query = self.query_data.get("q", "")  # Obtiene el parámetro 'q' del QueryString
+        session_id = self.get_book_session()
+        
+        # Realiza la búsqueda en los libros y devuelve resultados coincidentes
+        matching_books = self.search_books_in_redis(query)
+    
+        # Genera la respuesta HTML con los resultados
+        response = self.generate_search_results_html(matching_books)
+    
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.set_book_cookie(session_id)
+        self.end_headers()
+        self.wfile.write(response.encode("utf-8"))
+    
+    def search_books_in_redis(self, query):
+        # Código para buscar libros en Redis según el término de búsqueda 'query'
+        # Debes implementar esta función para buscar en Redis y devolver los resultados
+        pass
+    
+    def generate_search_results_html(self, matching_books):
+        # Genera el HTML de los resultados de búsqueda
+        # Puedes utilizar BeautifulSoup para construir la estructura HTML
+        # Debes implementar esta función para generar la respuesta HTML
+        pass
+    
+    # Agrega el nuevo patrón de ruta para la búsqueda
+    mapping.append((r'^/search$', 'search_books'))
+###################################################################################
     @cached_property
     def url(self):
         return urlparse(self.path)
